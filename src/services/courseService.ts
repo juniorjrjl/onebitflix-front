fa365b86228607586ed8b02b2216c565a6985cdd
Junior <junior.jr.jl@gmail.com>
2022-12-29 18:44:08 -0300

    pagina home após login

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -13,11 +13,35 @@ export type NewestCourse = {
     updatedAt: Date
 };
 
+export type FeaturedCourse ={
+    id: number
+    name: string
+    synopsis: string
+    thumbnailUrl: string
+    featured: boolean
+    CategoryId: number
+    createdAt: Date
+    updatedAt: Date
+}
+
 const courseService ={
 
     getNewest :async () => {
         try{
             const res = await api.get<AxiosResponse<NewestCourse[]>>(`${process.env.NEXT_PUBLIC_BASEURL_INTERNAL!}/courses/newest`)
+            return res
+        } catch(err) {
+            if (!axios.isAxiosError<AxiosError<ErrorType>>(err)) throw err
+
+            console.error(err)
+            return err.response!
+        }
+    },
+
+    getFeatured : async () => {
+        try{
+            const Authorization = `Bearer ${sessionStorage.getItem("onebitflix-token")}`
+            const res = await api.get<AxiosResponse<FeaturedCourse[]>>(`/courses/featured`,{headers: { Authorization }})
             return res
         } catch(err) {
             if (!axios.isAxiosError<AxiosError<ErrorType>>(err)) throw err
