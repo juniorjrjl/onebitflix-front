30d96abda0290501160f061a5af4817ffea4dd28
Junior <junior.jr.jl@gmail.com>
2023-01-03 12:46:44 -0300

    cards da pesquisa de cursos

--- pages/search.tsx
+++ pages/search.tsx
@@ -5,6 +5,9 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { Page } from '../src/services/api';
 import courseService, { PagedCourses } from '../src/services/courseService';
+import { Container } from 'reactstrap';
+import SearchCard from '../src/components/searchCard';
+import Footer from '../src/components/common/footer';
 
 const Search = () =>{
     const router = useRouter();
@@ -22,13 +25,23 @@ const Search = () =>{
             <title>Onebitflix - {searchName}</title>
             <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon"/>
         </Head>
-        <main>
-            <HeaderAuth />
-            {searchResult?.content?.map((course: PagedCourses) =>(
-                <div key={course.id}>
-                    <p>{course.name}</p>
-                </div>
-            ))}
+        <main className={styles.main}>
+            <div className={styles.headerFooterBG}>
+                <HeaderAuth />
+            </div>
+            <div className={styles.searchResult}>
+                <Container className='d-flex flex-wrap justify-content-center gap-5 py-4'>
+                    {searchResult?.content.length >= 1 ? 
+                        searchResult?.content?.map((course: PagedCourses) =>(
+                                <SearchCard key={course.id} course={course}/>
+                        )) :
+                        <p className={styles.noSearchResult}>Nenhum resultado encontrado</p>
+                    }
+                </Container>
+            </div>
+            <div className={styles.headerFooterBG}>
+                <Footer />
+            </div>
         </main>
     </>)
 }
